c663ae120273ab4b215e224facf9e8e67553c950
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2025-01-09 18:46:45 +0530

    Add .gitignore, update requirements, and enhance Jupyter notebook with analysis sections

--- app.py
+++ app.py
@@ -12,56 +12,73 @@ import matplotlib.pyplot as plt
 
 def lb(n=1):  # line breaks
     for _ in range(n):
-        st.markdown('''
+        st.markdown(
+            """
         <br>
-        ''', unsafe_allow_html=True)
+        """,
+            unsafe_allow_html=True,
+        )
 
 
 def page_header(title, color, size=2.8, top=100, bottom=50):
-    st.markdown(f"""
+    st.markdown(
+        f"""
     <h1 style="text-align: center; color: {color}; font-size: {size}rem; margin-top: -{top}px; margin-bottom: -{bottom}px">
     {title}
     </h1>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
 
 
 def text(txt):
-    st.markdown(f"""
+    st.markdown(
+        f"""
     <p style="text-align: center; font-size: 1.5rem">
     {txt}
     </p>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
 
 
 # Dataset
 
-df = pd.read_csv('resources/startup_cleaned.csv')
+df = pd.read_csv("resources/startup_cleaned.csv")
 
 # Page Configuration ...
 
 ch_color = "#0e1117"
-st.set_page_config(layout='wide', page_title='Startup Analysis', page_icon='📊')
+st.set_page_config(layout="wide", page_title="Startup Analysis", page_icon="📊")
 
 
 # ---------------------------------- C O N T E N T ---------------------------------- #
 
 
 def main():
-    page_header(title='Indian Startup Funding Dashboard',
-                color='lightblue', size=2.5, top=80)
+    page_header(
+        title="Indian Startup Funding Dashboard", color="lightblue", size=2.5, top=80
+    )
     lb(3)
-    c1, c2, c3 = st.columns((3, 8, 3), gap='medium')
+    c1, c2, c3 = st.columns((3, 8, 3), gap="medium")
     with c2:
-        st.image('resources/dataset-cover.jpg',
-                 caption='Dataset cover image', use_column_width='auto')
+        st.image(
+            "resources/dataset-cover.jpg",
+            caption="Dataset cover image",
+            use_column_width="auto",
+        )
         lb()
-        st.markdown("""
+        st.markdown(
+            """
         ##### A detailed analysis of Indian Startups based on a **Kaggle** dataset.
         Choose an analysis option from the **dropdown menu** in the sidebar to view the corresponding details. 📊
-        """)
-        st.markdown("""
+        """
+        )
+        st.markdown(
+            """
         > Dataset link - [Indian Startup Funding](https://www.kaggle.com/datasets/sudalairajkumar/indian-startup-funding)
-        """)
+        """
+        )
 
 
 def overall():
@@ -71,52 +88,51 @@ def overall():
 
     total = round(df.amount.sum() / 10000)
 
-    max = round(sdf.max().sort_values(
-        ascending=False).head(1).values[0] / 10000)
+    max = round(sdf.max().sort_values(ascending=False).head(1).values[0] / 10000)
 
     mean = round(sdf.sum().mean() / 100)
 
     # Rendered items ...
 
-    page_header(title="Overall Analysis",
-                color="lightblue", size=2.25)
+    page_header(title="Overall Analysis", color="lightblue", size=2.25)
 
     # C O L U M N S ...
 
-    c1, c2, c3 = st.columns(3, gap='large')
+    c1, c2, c3 = st.columns(3, gap="large")
 
     with c1:
-        st.metric('Total Investment', str(total) + 'Cr')
+        st.metric("Total Investment", str(total) + "Cr")
     with c2:
-        st.metric('Maximum Investment', str(max) + 'Cr')
+        st.metric("Maximum Investment", str(max) + "Cr")
     with c3:
-        st.metric('Average Investment', str(mean) + 'Cr')
+        st.metric("Average Investment", str(mean) + "Cr")
 
-    c4, padding, c5 = st.columns((10, 2, 10), gap='medium')
-    c6, padding, c7 = st.columns((10, 2, 10), gap='medium')
+    c4, padding, c5 = st.columns((10, 2, 10), gap="medium")
+    c6, padding, c7 = st.columns((10, 2, 10), gap="medium")
 
     # LINE - CHART ...
 
     with c4:
-        st.subheader('Month on Month Graph')
+        st.subheader("Month on Month Graph")
 
-        opt = st.selectbox('Select the type of aggregation', [
-            'Total Investment', 'Investment Count'])
+        opt = st.selectbox(
+            "Select the type of aggregation", ["Total Investment", "Investment Count"]
+        )
 
         if opt == "Total Investment":
             tdf = Data.momg1(df)
 
             # Graph 1
-            st.line_chart(data=tdf, x='Month', y='Amount in Crores')
+            st.line_chart(data=tdf, x="Month", y="Amount in Crores")
 
         elif opt == "Investment Count":
             tdf2 = Data.momg2(df)
-            st.line_chart(data=tdf2, x='Month', y='Count of investments')
+            st.line_chart(data=tdf2, x="Month", y="Count of investments")
 
     # PIE CHART ...
 
     with c5:
-        st.subheader('Top 5 Sectors')
+        st.subheader("Top 5 Sectors")
         cp = Data.catpie(df)
         top5 = px.pie(
             labels=cp.index.values,
@@ -125,21 +141,21 @@ def overall():
             height=400,
             width=500,
             color_discrete_sequence=px.colors.sequential.Tealgrn_r,
-            hole=0.4
+            hole=0.4,
         )
         st.plotly_chart(top5)
 
     with c6:
-        st.subheader('Top 5 Startups')
+        st.subheader("Top 5 Startups")
 
-        year1 = st.slider('Choose year for Startups', 2015, 2020, 2017)
+        year1 = st.slider("Choose year for Startups", 2015, 2020, 2017)
         top5 = Data.top5st(df, year1)
         st.dataframe(top5, width=600)
 
     with c7:
-        st.subheader('Top 5 Investors')
+        st.subheader("Top 5 Investors")
 
-        year2 = st.slider('Choose year for Investors', 2015, 2020, 2017)
+        year2 = st.slider("Choose year for Investors", 2015, 2020, 2017)
         top5 = Data.top5inv(df, year2)
         st.dataframe(top5, width=600)
 
@@ -150,18 +166,18 @@ def startups(btn1, df, startup):
         rnd = Data.stg(df, startup)
         inv = Data.sinv(df, startup)
 
-        pad1, col, pad2 = st.columns((2, 8, 2), gap='small')
+        pad1, col, pad2 = st.columns((2, 8, 2), gap="small")
 
         with col:
             st.header(startup)
             st.divider()
-            st.subheader('Overall details')
+            st.subheader("Overall details")
             st.dataframe(dfs)
 
-            c1, pad, c2 = st.columns((10, 1, 10), gap='small')
+            c1, pad, c2 = st.columns((10, 1, 10), gap="small")
 
             with c1:
-                st.subheader('Stage')
+                st.subheader("Stage")
                 st.dataframe(rnd)
 
             with c2:
@@ -171,7 +187,8 @@ def startups(btn1, df, startup):
     else:
         lb(2)
         text(
-            "Select a <b style='color:lightblue'>startup</b> name from the dropdown menu located in the <b>sidebar</b> to view their details.")
+            "Select a <b style='color:lightblue'>startup</b> name from the dropdown menu located in the <b>sidebar</b> to view their details."
+        )
 
 
 def investors(btn2, df, investor):
@@ -179,22 +196,21 @@ def investors(btn2, df, investor):
         st.header(investor)
         st.divider()
 
-        col1, padd, col2 = st.columns((7, 2, 7), gap='small')
-        col3, padd, col4 = st.columns((7, 2, 7), gap='small')
-        col5, padd, col6 = st.columns((7, 2, 7), gap='small')
+        col1, padd, col2 = st.columns((7, 2, 7), gap="small")
+        col3, padd, col4 = st.columns((7, 2, 7), gap="small")
+        col5, padd, col6 = st.columns((7, 2, 7), gap="small")
 
         with col1:
             recent = Data.mrinv(df, investor)
-            st.subheader('Most recent investments')
+            st.subheader("Most recent investments")
             st.dataframe(recent)
 
         with col2:
             top5 = Data.biginv(df, investor)
-            st.subheader('Top 5 investments')
+            st.subheader("Top 5 investments")
 
             fig, ax = plt.subplots(figsize=(6, 4))
-            ax.bar(top5['Startup'], top5['Amount in Crores'],
-                   color="lightblue")
+            ax.bar(top5["Startup"], top5["Amount in Crores"], color="lightblue")
 
             # Chart Color Customization ...
 
@@ -203,15 +219,15 @@ def investors(btn2, df, investor):
 
             # Chart LEGEND colors ...
 
-            ax.tick_params(axis='x', colors='white', labelsize=8)
-            ax.tick_params(axis='y', colors='white', labelsize=8)
+            ax.tick_params(axis="x", colors="white", labelsize=8)
+            ax.tick_params(axis="y", colors="white", labelsize=8)
 
             # Chart BORDER colors ...
 
-            ax.spines['top'].set_color(ch_color)
-            ax.spines['bottom'].set_color('white')
-            ax.spines['left'].set_color('white')
-            ax.spines['right'].set_color(ch_color)
+            ax.spines["top"].set_color(ch_color)
+            ax.spines["bottom"].set_color("white")
+            ax.spines["left"].set_color("white")
+            ax.spines["right"].set_color(ch_color)
 
             plt.xlabel("Startup", color="white", fontsize=10)
             plt.ylabel("Amount in Crores", color="white", fontsize=10)
@@ -228,21 +244,22 @@ def investors(btn2, df, investor):
                 hole=0.4,
                 height=400,
                 width=500,
-                color_discrete_sequence=px.colors.sequential.Teal
+                color_discrete_sequence=px.colors.sequential.Teal,
             )
             st.plotly_chart(pie1)
 
         with col4:
             city = Data.city(df, investor)
             st.subheader("Cities of investment")
-            pie3 = px.pie(labels=city.index.values,
-                          values=city.values,
-                          names=city.index.values,
-                          hole=0.4,
-                          height=400,
-                          width=500,
-                          color_discrete_sequence=px.colors.sequential.Darkmint_r
-                          )
+            pie3 = px.pie(
+                labels=city.index.values,
+                values=city.values,
+                names=city.index.values,
+                hole=0.4,
+                height=400,
+                width=500,
+                color_discrete_sequence=px.colors.sequential.Darkmint_r,
+            )
             st.plotly_chart(pie3)
 
         with col5:
@@ -255,30 +272,32 @@ def investors(btn2, df, investor):
                 hole=0.4,
                 height=400,
                 width=500,
-                color_discrete_sequence=px.colors.sequential.Blugrn_r
+                color_discrete_sequence=px.colors.sequential.Blugrn_r,
             )
             st.plotly_chart(pie2)
 
         with col6:
             yearly = Data.yrinv(df, investor)
             st.subheader("Year on year investment")
-            st.line_chart(yearly, y='Amount in Crores')
+            st.line_chart(yearly, y="Amount in Crores")
 
     else:
         lb(2)
         text(
-            "Select an <b style='color:lightblue'>investor</b> name from the dropdown menu located in the <b>sidebar</b> to view their details.")
+            "Select an <b style='color:lightblue'>investor</b> name from the dropdown menu located in the <b>sidebar</b> to view their details."
+        )
 
 
 # ---------------------------------- S I D E B A R ---------------------------------- #
 
 
-st.sidebar.title('Startup Funding Analysis')
+st.sidebar.title("Startup Funding Analysis")
 
 lb()
 
-option = st.sidebar.selectbox('Choose analysis option', [
-    "Data", "Overall Analysis", "Startups", "Investors"])
+option = st.sidebar.selectbox(
+    "Choose analysis option", ["Data", "Overall Analysis", "Startups", "Investors"]
+)
 
 if option == "Data":
     main()
@@ -286,13 +305,11 @@ elif option == "Overall Analysis":
     overall()
 elif option == "Startups":
     page_header(title="Startup Analysis", color="lightgreen", size=2.5)
-    startup = st.sidebar.selectbox(
-        "Select a startup", df['startup'].unique().tolist())
-    btn1 = st.sidebar.button('Find Startup details')
+    startup = st.sidebar.selectbox("Select a startup", df["startup"].unique().tolist())
+    btn1 = st.sidebar.button("Find Startup details")
     startups(btn1, df, startup)
 elif option == "Investors":
     page_header(title="Investor Analysis", color="lightgreen", size=2.5)
-    investor = st.sidebar.selectbox(
-        "Select an investor", Data.investor_list(df))
-    btn2 = st.sidebar.button('Find Investor details')
+    investor = st.sidebar.selectbox("Select an investor", Data.investor_list(df))
+    btn2 = st.sidebar.button("Find Investor details")
     investors(btn2, df, investor)
--- data_fetch.py
+++ data_fetch.py
@@ -5,7 +5,7 @@ class Data:
 
     @staticmethod
     def investor_list(df):
-        il = sorted(set(df['investors'].str.split(', ').sum()))
+        il = sorted(set(df["investors"].str.split(", ").sum()))
         return il
 
     # ------------------------------------- Page 1 ------------------------------------- #
@@ -14,33 +14,32 @@ class Data:
 
     @staticmethod
     def momg1(df):
-        tdf = df.groupby(['year', 'month'])['amount'].sum().reset_index()
-        tdf['Y'] = tdf['amount'] / 10000
-        tdf.rename(columns={'Y': 'Amount in Crores'}, inplace=True)
+        tdf = df.groupby(["year", "month"])["amount"].sum().reset_index()
+        tdf["Y"] = tdf["amount"] / 10000
+        tdf.rename(columns={"Y": "Amount in Crores"}, inplace=True)
         for i in range(tdf.shape[0]):
-            tdf.at[i,
-                   'Month'] = f"{tdf.at[i, 'month'][:3]} '{tdf.at[i, 'year'] - 2000}"
-        tdf.drop(columns=['year', 'month'], inplace=True)
+            tdf.at[i, "Month"] = f"{tdf.at[i, 'month'][:3]} '{tdf.at[i, 'year'] - 2000}"
+        tdf.drop(columns=["year", "month"], inplace=True)
         return tdf
 
     # for Line Chart - Option 2
 
     @staticmethod
     def momg2(df):
-        tdf2 = df.groupby(['year', 'month'])[
-            'amount'].count().reset_index()
-        tdf2.rename(columns={'amount': 'Count of investments'}, inplace=True)
+        tdf2 = df.groupby(["year", "month"])["amount"].count().reset_index()
+        tdf2.rename(columns={"amount": "Count of investments"}, inplace=True)
         for i in range(tdf2.shape[0]):
-            tdf2.at[i,
-                    'Month'] = f"{tdf2.at[i, 'month'][:3]} '{tdf2.at[i, 'year'] - 2000}"
-        tdf2.drop(columns=['year', 'month'], inplace=True)
+            tdf2.at[i, "Month"] = (
+                f"{tdf2.at[i, 'month'][:3]} '{tdf2.at[i, 'year'] - 2000}"
+            )
+        tdf2.drop(columns=["year", "month"], inplace=True)
         return tdf2
 
     # for Pie Chart ...
 
     @staticmethod
     def catpie(df):
-        cat = df.groupby('vertical')['amount'].sum()
+        cat = df.groupby("vertical")["amount"].sum()
         cat = cat.sort_values(ascending=False)
         return cat.head()
 
@@ -48,27 +47,40 @@ class Data:
 
     @staticmethod
     def top5st(df, y):
-        top5 = df.groupby(['startup', 'year', 'month'])[
-            'amount'].sum().reset_index()
-        top5['amount'] = top5['amount'] / 10000
-        top5.rename(columns={'amount': "Amount in Cr.",
-                             'month': 'Month', 'startup': 'Startup'}, inplace=True)
-        top5.set_index('Startup', inplace=True)
-        return top5.query(f"year == {y}")[['Month', 'Amount in Cr.']].head().sort_values(by='Amount in Cr.',
-                                                                                         ascending=False).head()
+        top5 = df.groupby(["startup", "year", "month"])["amount"].sum().reset_index()
+        top5["amount"] = top5["amount"] / 10000
+        top5.rename(
+            columns={"amount": "Amount in Cr.", "month": "Month", "startup": "Startup"},
+            inplace=True,
+        )
+        top5.set_index("Startup", inplace=True)
+        return (
+            top5.query(f"year == {y}")[["Month", "Amount in Cr."]]
+            .head()
+            .sort_values(by="Amount in Cr.", ascending=False)
+            .head()
+        )
 
     # for Top 5 Investors DF ...
 
     @staticmethod
     def top5inv(df, y):
-        top5 = df.groupby(['investors', 'year', 'month'])[
-            'amount'].sum().reset_index()
-        top5['amount'] = top5['amount'] / 10000
-        top5.rename(columns={'amount': "Amount in Cr.",
-                             'month': 'Month', 'investors': 'Investor'}, inplace=True)
-        top5.set_index('Investor', inplace=True)
-        return top5.query(f"year == {y}")[['Month', 'Amount in Cr.']].sort_values(by='Amount in Cr.',
-                                                                                  ascending=False).head()
+        top5 = df.groupby(["investors", "year", "month"])["amount"].sum().reset_index()
+        top5["amount"] = top5["amount"] / 10000
+        top5.rename(
+            columns={
+                "amount": "Amount in Cr.",
+                "month": "Month",
+                "investors": "Investor",
+            },
+            inplace=True,
+        )
+        top5.set_index("Investor", inplace=True)
+        return (
+            top5.query(f"year == {y}")[["Month", "Amount in Cr."]]
+            .sort_values(by="Amount in Cr.", ascending=False)
+            .head()
+        )
 
     # ------------------------------------- Page 2 ------------------------------------- #
 
@@ -78,26 +90,26 @@ class Data:
 
     @staticmethod
     def sup(df, name):
-        res = df[df['startup'] == name][['date', 'vertical',
-                                         'subvertical', 'city', 'round', 'amount']]
-        res.rename(columns={'amount': 'amount in Cr.'}, inplace=True)
-        res.set_index('date', inplace=True)
+        res = df[df["startup"] == name][
+            ["date", "vertical", "subvertical", "city", "round", "amount"]
+        ]
+        res.rename(columns={"amount": "amount in Cr."}, inplace=True)
+        res.set_index("date", inplace=True)
         return res.head()
 
     # Startup round investment ...
 
     @staticmethod
     def stg(df, name):
-        df.rename(columns={'amount': 'amount in Cr.'}, inplace=True)
-        res = df[df['startup'] == name].groupby(
-            'round')['amount in Cr.'].sum()
+        df.rename(columns={"amount": "amount in Cr."}, inplace=True)
+        res = df[df["startup"] == name].groupby("round")["amount in Cr."].sum()
         return res.head()
 
     # Startup investor(s) ...
 
     @staticmethod
     def sinv(df, name):
-        res = df[df['startup'] == name]['investors']
+        res = df[df["startup"] == name]["investors"]
         res = res.reset_index(drop=True)
         res.index = res.index + 1
         return res
@@ -110,21 +122,25 @@ class Data:
 
     @staticmethod
     def mrinv(df, name):
-        recent = df[df['investors'].str.contains(name)][['date', 'startup', 'vertical',
-                                                         'city', 'round', 'amount']].sort_values(by='date',
-                                                                                                 ascending=False)
-        recent.rename(columns={'amount': 'amount in Cr.'}, inplace=True)
-        recent.set_index('date', inplace=True)
+        recent = df[df["investors"].str.contains(name)][
+            ["date", "startup", "vertical", "city", "round", "amount"]
+        ].sort_values(by="date", ascending=False)
+        recent.rename(columns={"amount": "amount in Cr."}, inplace=True)
+        recent.set_index("date", inplace=True)
         return recent.head()
 
     # Biggest investments ...  B A R
 
     @staticmethod
     def biginv(df, name):
-        df.rename(columns={'amount': 'Amount in Crores',
-                           'startup': 'Startup'}, inplace=True)
-        big = df[df['investors'].str.contains(name)].groupby('Startup')[
-            'Amount in Crores'].sum()
+        df.rename(
+            columns={"amount": "Amount in Crores", "startup": "Startup"}, inplace=True
+        )
+        big = (
+            df[df["investors"].str.contains(name)]
+            .groupby("Startup")["Amount in Crores"]
+            .sum()
+        )
 
         big.sort_values(inplace=True, ascending=False)
 
@@ -136,33 +152,46 @@ class Data:
 
     @staticmethod
     def secinv(df, name):
-        sec = df[df['investors'].str.contains(name)].groupby(
-            'vertical')[
-            'Amount in Crores'].sum().head()
+        sec = (
+            df[df["investors"].str.contains(name)]
+            .groupby("vertical")["Amount in Crores"]
+            .sum()
+            .head()
+        )
         return sec
 
     # Stages invested ...   P I E  # 2
 
     @staticmethod
     def stginv(df, name):
-        stg = df[df['investors'].str.contains(
-            name)].groupby('round')['Amount in Crores'].sum().head()
+        stg = (
+            df[df["investors"].str.contains(name)]
+            .groupby("round")["Amount in Crores"]
+            .sum()
+            .head()
+        )
         return stg
 
     # Cities ...            P I E  # 3
 
     @staticmethod
     def city(df, name):
-        ct = df[df['investors'].str.contains(name)].groupby('city')[
-            'Amount in Crores'].sum().head()
+        ct = (
+            df[df["investors"].str.contains(name)]
+            .groupby("city")["Amount in Crores"]
+            .sum()
+            .head()
+        )
         return ct
 
     # Year on Year investment ...
 
     @staticmethod
     def yrinv(df, name):
-        df.rename(columns={'amount': 'Amount in Crores',
-                           'year': 'Year'}, inplace=True)
-        yi = df[df['investors'].str.contains(name)].groupby('Year')[
-            'Amount in Crores'].sum()
+        df.rename(columns={"amount": "Amount in Crores", "year": "Year"}, inplace=True)
+        yi = (
+            df[df["investors"].str.contains(name)]
+            .groupby("Year")["Amount in Crores"]
+            .sum()
+        )
         return yi
